c4001da96432cc4a39bfc4c30a1f229da68007e1
Spencer Miller <spence.miller@gmail.com>
2023-11-05 19:42:31 -0800

    stub out State functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,46 @@ use wasm_bindgen::prelude::*;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder, dpi::PhysicalSize,
+    window::{WindowBuilder, Window},
+    dpi::PhysicalSize,
 };
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    // window must be declared after the surface (todo: why?)
+    window: Window,
+}
+
+impl State {
+    // Async is required for the creation of some of the wgpu types
+    async fn new(window: Window) -> Self {
+        todo!()
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
+}
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
 pub fn run() {
